5ee1fc0bb211f5db73d20fd2087ef1286b1ead41
vucalur <vucalur.work+git@gmail.com>
2016-02-11 13:36:54 +0100

    feat(layout): Material Design layout with sidenav

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,5 +1,16 @@
 export class MainController {
-  constructor() {
+  constructor($mdSidenav) {
     'ngInject';
+
+    this.$mdSidenav = $mdSidenav;
+    this.selected = null;
+  }
+
+  toggleList() {
+    this.$mdSidenav('left').toggle();
+  }
+
+  selectItem(item) {
+    this.selected = item;
   }
 }
