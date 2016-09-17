4778a2e69318d46dfac38037acb64656c83171d5
vucalur <vucalur.work+git@gmail.com>
2016-09-17 12:02:05 +0200

    fix(channel): Fix error when launching app with an URL pointing directly to a channel - 'channels.channel' state
    * Caused by $firebaseObject() not returning a promise, but returning undefined immediately. FirebaseObject.$loaded() has to be used instead.
    * Introduced by: bd3c9b1 vucalur on 28.08.16 at 13:39 "feat(articles): linking parsing articles to channel in place of sample parsing demo"
    * TODO: performance. See code comment for details
    
    Closes #13

--- src/app/channels/_.states.js
+++ src/app/channels/_.states.js
@@ -14,7 +14,11 @@ export function routerConfig($stateProvider) {
       url: 'channel/{channelId}',
       component: 'channel',
       resolve: {
-        channel: (user, $transition$, Channels) => Channels.getChannel(user.uid, $transition$.params().channelId)
+        channel: (user, $transition$, Channels) => {
+          const firebaseObj = Channels.getChannel(user.uid, $transition$.params().channelId);
+          // TODO(vucalur): performance: don't wait with loading the rest of the app. Returning promise here blocks entire UI
+          return firebaseObj.$loaded(); // return promise
+        }
       }
     });
 }
