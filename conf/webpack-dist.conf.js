c857b92d3d13a6f89f4e936edc705f3cce53c6a9
vucalur <vucalur.work+git@gmail.com>
2016-09-21 12:47:38 +0200

    fix(gulp): Fix "Tried to load angular more than once." causing dist version to break

--- conf/webpack-dist.conf.js
+++ conf/webpack-dist.conf.js
@@ -57,6 +57,8 @@ module.exports = {
     new webpack.optimize.UglifyJsPlugin({
       compress: {unused: true, dead_code: true} // eslint-disable-line camelcase
     }),
+    // Prevent vendor dulpication
+    new webpack.optimize.CommonsChunkPlugin({name: 'vendor'}),
     new ExtractTextPlugin('index-[contenthash].css')
   ],
   postcss: () => [autoprefixer],
